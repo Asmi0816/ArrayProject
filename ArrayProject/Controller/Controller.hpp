e0d23cea8020e564da73a87afe987cfd632a6f0f
Asmi0816 <asmi0816@740s-pl-114.local>
2017-02-06 12:26:49 -0700

    New array project was made.

---
+++ ArrayProject/Controller/Controller.hpp
@@ -0,0 +1,21 @@
+//
+//  Controller.hpp
+//  ArrayProject
+//
+//  Created by Smith, Arick on 2/6/17.
+//  Copyright © 2017 Smith, Arick. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+class Controller
+{
+private:
+    int nodeData();
+    Controller * nodePointer();
+public:
+    void start();
+};
+
+#endif /* Controller_hpp */
---
+++ ArrayProject/Controller/Runner.cpp
@@ -0,0 +1,16 @@
+//
+//  Runner.cpp
+//  ArrayProject
+//
+//  Created by Smith, Arick on 2/6/17.
+//  Copyright © 2017 Smith, Arick. All rights reserved.
+//
+
+#include "Controller.hpp"
+int main()
+{
+    Controller * myController = new Controller();
+    myController->start();
+    
+    return 0;
+}
